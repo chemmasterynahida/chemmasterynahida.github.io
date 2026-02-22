9b1f3bf594ddab91e8033085a3f0a38535862fc6
chemmasterynahida <chemmasterynahida@gmail.com>
2026-02-22 21:05:14 +0900

    site renovation

---
+++ src/config/category.ts
@@ -0,0 +1,6 @@
+export const CATEGORY_IMAGE_MAP: Record<string, string> = {
+  "Quantum Chemistry": "https://gcore.jsdelivr.net/gh/chemmasterynahida/assets/images/nahida-wp.png"
+};
+
+export const DEFAULT_CATEGORY_IMAGE =
+  "https://gcore.jsdelivr.net/gh/chemmasterynahida/assets/images/nahida.png";
